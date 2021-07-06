02d71fa6ab8dbaa1ac530bcfb2719439d7f4eea1
sanggoalist <s.tran@goalist.co.jp>
2021-07-06 09:26:40 +0700

    Change to use script file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,21 +12,11 @@ class MyDocument extends NextDocument {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta name="google-site-verification" content="W8kc4WlU1OO_I76CmgndqrjA0JXtU4N3I_HrigWRaSE" />
-          <script>
-              {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-WM899BC');`}
+          <script src = "assets/vendors/google-verification.js" type = "text/javascript">
           </script>
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159445034-1">
           </script>
-          <script>
-              {`window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'UA-159445034-1');`}
+          <script src = "assets/vendors/gtag.js" type = "text/javascript">
           </script>
         </Head>
         <body>
