291b9a2083149316f8f70f7a5785abeb3132a407
sanggoalist <s.tran@goalist.co.jp>
2021-07-06 10:32:10 +0700

    gtag fixing

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,21 +2,35 @@ import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends NextDocument {
   static async getInitialProps(ctx) {
+    
     const initialProps = await NextDocument.getInitialProps(ctx)
     return { ...initialProps }
   }
-  
+
   render() {
+    
     return (
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta name="google-site-verification" content="W8kc4WlU1OO_I76CmgndqrjA0JXtU4N3I_HrigWRaSE" />
-          <script src = "assets/vendors/google-verification.js" type = "text/javascript">
+          <script dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WM899BC');`,
+            }}>
           </script>
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-N1C7Y4ZLLD">
           </script>
-          <script src = "assets/vendors/gtag.js" type = "text/javascript">
+          <script dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              
+              gtag('config', 'G-N1C7Y4ZLLD');`,
+            }} >
           </script>
         </Head>
         <body>
